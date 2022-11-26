f251a8bc8543e17a9a6fc03f33529db22a0f94f1
Jeiku Dev <shippai.jeikudev@gmail.com>
2022-11-26 15:02:44 +0800

    master setup

--- App.tsx
+++ App.tsx
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022 / JeikuDev
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
